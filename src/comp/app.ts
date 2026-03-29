ed1dfc76c85a09b2723c70bfa58026da4f7d11de
x1n13y84issmd42 <x1n13y84issmd42@gmail.com>
2026-03-29 16:45:09 +0300

    Alert for missing Metamask.

--- src/comp/app.ts
+++ src/comp/app.ts
@@ -8,30 +8,32 @@ export type State = Partial<{
 }>;
 
 export async function stateFn(state: State) {
-	const web3 = new Web3((window as any).ethereum);
-	state.web3 = web3;
+	if ((window as any).ethereum) {
+		const web3 = new Web3((window as any).ethereum);
+		state.web3 = web3;
+		
+		state.account = state.account || (await web3.eth.getAccounts())[0];
 	
-	state.account = state.account || (await web3.eth.getAccounts())[0];
-
-	state.authorize = function () {
-		web3.eth.requestAccounts()
-			.then(accs => state.account = accs[0])
-			.catch(err => console.error(err))
-		;
-
-		(window as any).ethereum.on('accountsChanged', (accounts: string[]) => {
-			if (accounts.length === 0) {
+		state.authorize = function () {
+			web3.eth.requestAccounts()
+				.then(accs => state.account = accs[0])
+				.catch(err => console.error(err))
+			;
+	
+			(window as any).ethereum.on('accountsChanged', (accounts: string[]) => {
+				if (accounts.length === 0) {
+					disconnect();
+				}
+			});
+	
+			(window as any).ethereum.on('disconnect', () => {
 				disconnect();
-			}
-		});
-
-		(window as any).ethereum.on('disconnect', () => {
-			disconnect();
-		});
-	}
-
-	function disconnect() {
-		state.account = false;
+			});
+		}
+	
+		function disconnect() {
+			state.account = false;
+		}
 	}
 
 	return state;
